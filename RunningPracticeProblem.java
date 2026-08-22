8f565bee8328cad44fa137d6bf6ecbbb41efdc65
Pavan <pavannadikatla1@gmail.com>
2026-08-22 10:46:27 +0530

    daily java practice 21-08-26

---
+++ RunningPracticeProblem.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        while (t-- > 0) {
+            int n = scanner.nextInt();
+            int[] a = new int[n];
+            int[] b = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                a[i] = scanner.nextInt();
+            }
+
+            for (int i = 0; i < n; i++) {
+                b[i] = scanner.nextInt();
+            }
+            int sum=0;
+            for(int i=0;i<n;i++)
+            {
+                if(a[i] <= 2 * b[i] && b[i] <= 2 * a[i])
+                     sum++;
+                      
+            }
+            System.out.println(sum);
+            
+        }
+    }
+}
